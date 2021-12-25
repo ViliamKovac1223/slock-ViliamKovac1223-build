b052a47e0d2863d52adc0fd7f6432c3b88ca95c8
ViliamKovac1223 <viliamkovac1223@gmail.com>
2021-12-25 15:31:43 +0100

    change configuration

--- config.def.h
+++ config.def.h
@@ -1,28 +1,28 @@
 /* user and group to drop privileges to */
-static const char *user  = "nobody";
-static const char *group = "nogroup";
+static const char *user  = "sorig";
+static const char *group = "sorig";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
+	[INIT] =   "#000000",     /* after initialization */
+	[INPUT] =  "#FF8C00",   /* during input */
+	[FAILED] = "#F07178",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
 
-static const char *imgpath          = "img.xpm";
-static const int imgwidth           = 1920;
-static const int imgheight          = 1080;
+static const char *imgpath          = "/home/sorig/Pictures/slock-image.xpm";
+static const int imgwidth           = 1366;
+static const int imgheight          = 768;
 static const int imgoffsetx         = 0;
 static const int imgoffsety         = 0;
 static const int showimgonlyatstart = 1;
 
 /* default message */
-static const char * message = "Suckless: Software that sucks less.";
+static const char * message = "It's locked for a reason.";
 
 /* text color */
-static const char * text_color = "#ffffff";
+static const char * text_color = "#FFFFFF";
 
 /* text size (must be a valid size) */
-static const char * font_name = "6x10";
+static const char * font_name = "6x13";
